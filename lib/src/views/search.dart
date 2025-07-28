bb218458a4363091e2c0d95887d96bd22d902d0e
iqbal <farisikbal304@gmail.com>
2025-07-28 16:20:50 +0700

    restyle page : search.dart

--- lib/src/views/search.dart
+++ lib/src/views/search.dart
@@ -4,7 +4,6 @@ import 'package:myapp/src/views/Profile.dart';
 import 'package:myapp/src/views/cart.dart';
 import 'package:myapp/src/views/dashboard.dart';
 import 'package:myapp/src/views/video.dart';
-import 'notificationpage.dart';
 
 class Search extends StatefulWidget {
   const Search({super.key});
@@ -27,7 +26,7 @@ class _SearchState extends State<Search> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Colors.amber,
 
       appBar: _selectedIndex == 0
           ? AppBar(
@@ -52,28 +51,10 @@ class _SearchState extends State<Search> {
                           border: OutlineInputBorder(
                             borderRadius: BorderRadius.circular(10),
                           ),
-                          suffixIcon: IconButton(
-                            icon: const Icon(
-                              Icons.camera_alt,
-                              color: Colors.black,
-                            ),
-                            onPressed: () {},
-                          ),
                           contentPadding: EdgeInsets.symmetric(horizontal: 10),
                         ),
                       ),
                     ),
-                  ),
-                  IconButton(
-                    icon: Icon(Icons.notifications, size: 30),
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => Notificationpage(),
-                        ),
-                      );
-                    },
                   ),
                   IconButton(
                     icon: Icon(Icons.chat, size: 30),
@@ -84,15 +65,175 @@ class _SearchState extends State<Search> {
             )
           : null,
 
-      body: _selectedIndex == 0
-          ? SingleChildScrollView(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [],
+      body: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(15),
+            topRight: Radius.circular(15),
+          ),
+          color: Colors.white,
+        ),
+        height: double.infinity,
+        child: _selectedIndex == 0
+            ? SingleChildScrollView(
+                padding: const EdgeInsets.all(5.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    categorylist(),
+
+                    SizedBox(height: 4),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Expanded(
+                          flex: 1,
+                          child: Column(
+                            children: [
+                              _postimage(true),
+                              _postimage(false),
+                              _postvideo(),
+                              _postimage(true),
+                              _postimage(false),
+                              _postimage(true),
+                              _postimage(false),
+                              _postvideo(),
+                              _postimage(true),
+                              _postimage(false),
+                            ],
+                          ),
+                        ),
+                        Expanded(
+                          flex: 1,
+                          child: Column(
+                            children: [
+                              _postimage(true),
+                              _postvideo(),
+                              _postimage(true),
+                              _postvideo(),
+                              _postimage(true),
+                              _postimage(false),
+                              _postvideo(),
+                              _postimage(true),
+                              _postimage(false),
+                            ],
+                          ),
+                        ),
+                        Expanded(
+                          flex: 1,
+                          child: Column(
+                            children: [
+                              _postimage(false),
+                              _postvideo(),
+                              _postvideo(),
+                              _postimage(true),
+                              _postimage(true),
+                              _postimage(false),
+                              _postvideo(),
+                              _postimage(true),
+                              _postimage(false),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              )
+            : _pages[_selectedIndex],
+      ),
+    );
+  }
+
+  SingleChildScrollView categorylist() {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: [
+          _categories('Celana oversize'),
+          _categories('Kosmetik'),
+          _categories('Kendaraan'),
+          _categories('Helm'),
+          _categories('Keyboard tactical'),
+          _categories('mouse'),
+          _categories('Lampu LED'),
+        ],
+      ),
+    );
+  }
+
+  Widget _categories(String category) {
+    return Container(
+      margin: EdgeInsets.only(right: 8),
+      decoration: BoxDecoration(
+        color: const Color.fromARGB(255, 223, 223, 223),
+        borderRadius: BorderRadius.circular(9),
+      ),
+      child: TextButton(
+        style: TextButton.styleFrom(
+          padding: EdgeInsets.only(left: 11, right: 11, top: 8, bottom: 8),
+          minimumSize: Size.zero,
+          tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+        ),
+        onPressed: () {},
+        child: Text(
+          category,
+          style: TextStyle(
+            fontWeight: FontWeight.w900,
+            fontSize: 15,
+            color: const Color.fromARGB(255, 59, 59, 59),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _postimage(bool isMany) {
+    return Container(
+      decoration: BoxDecoration(
+        color: const Color.fromARGB(255, 227, 227, 227),
+      ),
+      margin: EdgeInsets.all(1),
+      width: double.infinity,
+      height: 130,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            if (isMany)
+              Icon(
+                Icons.collections,
+                color: const Color.fromARGB(255, 174, 174, 174),
               ),
-            )
-          : _pages[_selectedIndex],
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _postvideo() {
+    return Container(
+      decoration: BoxDecoration(
+        color: const Color.fromARGB(255, 227, 227, 227),
+      ),
+      margin: EdgeInsets.all(1),
+      width: double.infinity,
+      height: 260,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Icon(
+              Icons.movie,
+              color: const Color.fromARGB(255, 174, 174, 174),
+              size: 28,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
